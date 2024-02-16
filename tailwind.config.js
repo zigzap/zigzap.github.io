6c0e0080a03dd2d048a89e2a771782c1b6dc3491
Brook Jeynes <jeynesbrook@gmail.com>
2024-02-16 13:05:13 +1000

    Updated site

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.html"],
+    theme: {
+        extend: {
+            colors: {
+                zap: {
+                    "yellow": "#FFD600",
+                    "red": "#CD4A4A",
+                }
+            }
+        },
+    },
+    plugins: [],
+}
